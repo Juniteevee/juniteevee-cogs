f060896333a8a4426a7e6e277dfda1e47cc3556e
Juni <juni@bunhun.fun>
2022-04-09 13:32:53 -0400

    Basic files

---
+++ reactquote/__init__.py
@@ -0,0 +1,5 @@
+from .reactquote import ReactQuote
+
+
+def setup(bot):
+    bot.add_cog(ReactQuote(bot))
---
+++ reactquote/reactquote.py
@@ -0,0 +1,13 @@
+from redbot.core import commands
+
+class ReactQuote(commands.Cog):
+    """Cog to store quotes by reacting with speech bubble"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def reactquote(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
