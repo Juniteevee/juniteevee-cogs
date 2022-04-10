7a2f6b6eeaf0eb8014bfef2b9d5eac67450548c1
Juni <juni@bunhun.fun>
2022-04-10 09:29:34 -0400

    more methods

--- reactquote/__init__.py
+++
@@ -1,5 +0,0 @@
-from .reactquote import ReactQuote
-
-
-def setup(bot):
-    bot.add_cog(ReactQuote(bot))
--- reactquote/reactquote.py
+++ reactquote/reactquote.py
@@ -1,13 +1,47 @@
 from redbot.core import commands
+from redbot.core.utils import fileIO
+from __main__ import send_cmd_help
+import os
+from random import choice as randchoice
 
 class ReactQuote(commands.Cog):
     """Cog to store quotes by reacting with speech bubble"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.quotes = fileIO("data/quotes/quotes.json", "load")
+
+    def _wrapQuote(self, msg):
+        return msg
 
     @commands.command()
     async def reactquote(self, ctx):
         """TestCommand"""
         # Your code will go here
         await ctx.send("I can do stuff, really!")
+
+    @commands.command()
+    async def addquote(self, *message):
+        """Manually Add Quote"""
+        self.bot.say(message)
+
+def check_folder():
+    if not os.path.exists("data/reactquote"):
+        print("Creating data/reactquote folder...")
+        os.makedirs("data/reactquote")
+
+
+def check_file(self):
+    quotes = {}
+
+    f = "data/reactquote/reactquote.json"
+    if not fileIO(f, "check"):
+        print("Creating default reactquote's reactquote.json...")
+        fileIO(f, "save", quotes)
+
+
+def setup(bot):
+    check_folder()
+    check_file()
+    n = ReactQuote(bot)
+    bot.add_cog(n)
