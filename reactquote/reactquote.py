42d3f78c2c8a70720add654f670fd57213cf644c
Juni <juni@bunhun.fun>
2022-04-10 14:59:07 -0400

    trying to fix stuff

--- reactquote/reactquote.py
+++ reactquote/reactquote.py
@@ -1,15 +1,10 @@
 from redbot.core import commands
-from redbot.core.utils import fileIO
-from __main__ import send_cmd_help
-import os
-from random import choice as randchoice
 
 class ReactQuote(commands.Cog):
     """Cog to store quotes by reacting with speech bubble"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.quotes = fileIO("data/quotes/quotes.json", "load")
 
     def _wrapQuote(self, msg):
         return msg
@@ -19,8 +14,3 @@ class ReactQuote(commands.Cog):
         """TestCommand"""
         # Your code will go here
         await ctx.send("I can do stuff, really!")
-
-    @commands.command()
-    async def addquote(self, *message):
-        """Manually Add Quote"""
-        self.bot.say(message)
