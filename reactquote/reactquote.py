3531e8736240ab745b7a114ec46bef889924326c
Juni <juni@bunhun.fun>
2022-04-10 16:01:49 -0400

    speech react test

--- reactquote/reactquote.py
+++ reactquote/reactquote.py
@@ -1,4 +1,5 @@
 from redbot.core import commands
+import discord
 
 class ReactQuote(commands.Cog):
     """Cog to store quotes by reacting with speech bubble"""
@@ -10,7 +11,20 @@ class ReactQuote(commands.Cog):
         return msg
 
     @commands.command()
-    async def reactquote(self, ctx):
+    async def reactquote(self, ctx: commands.Context):
         """TestCommand"""
         # Your code will go here
         await ctx.send("I can do stuff, really!")
+        
+
+    @commands.command()
+    async def quote(self, ctx: commands.Context):
+        """TestCommand"""
+        # Your code will go here
+        await ctx.send("I can do stuff, really!")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload:discord.RawReactionActionEvent):
+        """On React"""
+        if str(payload.emoji) == ':speech_ballon':
+            self.bot.get_channel(payload.channel_id).send('speech')
