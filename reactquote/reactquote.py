332c534ca28ebda9a43a41c88af421339b0e3b5e
Juni <juni@bunhun.fun>
2022-04-11 10:25:05 -0400

    clean up add message

--- reactquote/reactquote.py
+++ reactquote/reactquote.py
@@ -33,4 +33,4 @@ class ReactQuote(commands.Cog):
             message: discord.Message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
             user = payload.member
             channel: discord.TextChannel = message.channel
-            await channel.send("I'd quote that if I could...")
+            await channel.send(f"New quote added by {user.display_name} as #1\n({message.jump_url})")
