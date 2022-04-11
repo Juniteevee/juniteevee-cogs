79c71ffa3904e7fd1431a1dcf130ee6908e0cbbb
Juni <juni@bunhun.fun>
2022-04-11 10:04:43 -0400

    author

--- reactquote/reactquote.py
+++ reactquote/reactquote.py
@@ -13,9 +13,10 @@ class ReactQuote(commands.Cog):
 
     def _buildQuote(self, message:discord.Message):
         
-        quote = "Quoted text will be here\n- <@{message.author.display_name}> [(Jump)]({message.jump_url})"
+        quote = "Quoted text will be here\n[(Jump)]({message.jump_url})"
         timestamp = datetime.now()
         embed = discord.Embed(timestamp=timestamp)
+        embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
         embed.add_field(name="#1", value=quote, inline=False)
         return embed
 
